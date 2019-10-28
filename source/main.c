5aae54d8ada079d8025459519d40afb395fa7560
Zion Gutierrez <zguti001@wch136-05.cs.ucr.edu>
2019-10-28 12:24:39 -0700

    Part 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: zguti001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #8  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,10 +12,22 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1<< ADSC) | (1<< ADATE);
+	}
 
-    /* Insert your solution below */
+
+unsigned short x = ADC;
+unsigned short my_short = 0xABCD;
+my_char = (char)(my_short >> 4); //my_char = 0xBC
+
+int main(void) {
+    DDRB = 0xFF; PORTB = 0x00; //output LEDs
+    DDRD = 0xFF; PORTD = 0x00; //output LEDs
+    DDRA = 0x00; PORTA = 0xFF; //input POTentiometer
+    
+    ADC_init();
+
     while (1) {
 
     }
